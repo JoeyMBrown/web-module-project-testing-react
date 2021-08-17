3eda19dfe864cefd284b2a96162b006768dbf961
Joey_M_Brown <joey.ma.brown@gmail.com>
2021-08-17 00:25:48 -0400

    Added fix

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -58,8 +58,7 @@ test('When fetch button is pressed, calls onClick function', async () => {
     userEvent.click(fetchButton);
 
     await waitFor(() => {
-        const mockLen = optionalFunction.mock.calls.length;
-        expect(mockLen).toBe(1);
+        expect(optionalFunction).toBeCalledTimes(1);
     })
 
 })
@@ -68,9 +67,6 @@ test('When fetch button is pressed, calls onClick function', async () => {
 //1. Add in nessisary imports and values to establish the testing suite.
 //2. Test that the Display component renders without any passed in props.
 //3. Rebuild or copy a show test data element as used in the previous set of tests.
-
 //4. Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.
-
 //5. Test that when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data.
-
 //6. Notice the optional functional prop passed in to the Display component client code. Test that when the fetch button is pressed, this function is called.
